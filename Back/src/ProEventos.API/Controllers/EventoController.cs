51f5e44229735b81e1931c084478d7aa50a654cf
Raul Rangel <raul.srfilho@hotmail.com>
2022-03-03 17:45:06 -0300

    First commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        
+        public IEnumerable<Evento> _evento = new Evento[] {
+            new Evento() {
+                EventoId = 1,
+                Tema = "Angular e Dotnet",
+                Local = "SP",
+                Lote = "rimeiro",
+                QtPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString()
+            },
+            new Evento() {
+                EventoId = 2,
+                Tema = "Angular 2 e Dotnet",
+                Local = "SP22",
+                Lote = "rimeiro2",
+                QtPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy")
+            
+            }
+
+            };
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+    }
+}
